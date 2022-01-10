ab70d2f6f766021b0f6741b58cce04a32ec22f9b
Biparnak Roy <roybiparnak@gmail.com>
2022-01-10 22:57:23 +0530

    adjusting devfolio button

--- src/components/1. HeroSection/index.js
+++ src/components/1. HeroSection/index.js
@@ -26,15 +26,17 @@ const HeroSection = () => {
                         <span>&nbsp;&nbsp;</span> <span style={{ color: "white" }}>Techno</span><span style={{ color: "#0692ce" }}>philia</span>
                     </HeroH1>
                     <HeroP>#Dare_to_Dream</HeroP>
-                    <HeroBtnWrapper>
-                        <div
-                             id="devfolio-apply-now"
-                             className="apply-button"
-                             data-hackathon-slug="doubleslash-ieee-jaduniv"
-                             data-button-theme="light"
-                             style={{height: "44px", width: "312px"}} >
+                    
+                    <div
+                        id="devfolio-apply-now"
+                        className="apply-button"
+                        data-hackathon-slug="test-ieee-jaduniv"
+                        data-button-theme="light"
+                        style={{ height: "44px", width: "300px", paddingBottom:"10px" }} >
 
-                        </div>
+                    </div>
+                    <br />
+                    <HeroBtnWrapper>
                         <Button
                             to="signup"
                             onMouseEnter={onHover}
@@ -42,7 +44,7 @@ const HeroSection = () => {
                             primary="true"
                             dark="true"
                             style={{ backgroundColor: hover ? "#0692ce" : "white" }}
-                        >Join Our Discord <img src={require("../../images/discord.png").default} width="32px" height="32px" alt="discord_new" style={{paddingLeft:"3px"}}/> {hover ? <ArrowForward /> : <ArrowRight />}</Button>
+                        >Join Our Discord <img src={require("../../images/discord.png").default} width="32px" height="32px" alt="discord_new" style={{ paddingLeft: "3px" }} /> {hover ? <ArrowForward /> : <ArrowRight />}</Button>
                     </HeroBtnWrapper>
                 </HeroContent>
             </HeroContainer>
