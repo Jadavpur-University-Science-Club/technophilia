0fc2578d466e26a3d78b70555c54b99e60d9385e
Arkadeep Sinha <sinhaarkadeep2014@gmail.com>
2022-01-22 15:04:53 +0530

    prizes 1.1
    imports commented out

--- src/components/5. Prizes/index.js
+++ src/components/5. Prizes/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { createTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
 import { Container, HeadingH1} from "../Commons"
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
+//import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
+//import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { Row, Col } from "react-grid-system";
 
